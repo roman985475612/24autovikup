31774ce65d55266bf5093b994c3885b97d883010
Roman <roma2@protonmail.com>
2020-07-09 19:34:49 +0300

    Updated styles and contacts

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
 $name = trim($_POST['name']);
 $phone = trim($_POST['phone']);
 
-$to = 'roman985475612@gmail.com';
+$to = 'ar.m2009@yandex.ru';
 $subject = 'Заявка от клиента';
 $message = 'Клиент '.$name.' просит перезвонить по номеру '.$phone;
 $headers  = 'Content-type: text/html; charset=windows-1251 \r\n'; 
